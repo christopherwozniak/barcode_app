eb6eae287729e13d24d987064471351d9de64cc5
Krzysztof Woźniak <wozniak.krzysztof124@gmail.com>
2022-07-07 14:11:44 +0200

    Change backgroundcolor

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: const Scaffold(backgroundColor: Colors.green),
     );
   }
 }
